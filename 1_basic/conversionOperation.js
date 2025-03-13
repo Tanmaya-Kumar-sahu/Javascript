89405435c6fd8b16358bfbe01cb1f21ff02a4650
Tanmaya-Kumar-sahu <tanmayakumarsahu456@gmail.com>
2025-03-13 05:37:59 +0000

    conversion datatypes

---
+++ 1_basic/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Tanmay"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 / false => 0
+
+
+let isLoggedin = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true / 0 => false
+//"" => false
+//"tanmay" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
