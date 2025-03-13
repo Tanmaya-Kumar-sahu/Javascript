467726ef9a43ca92c24554192e82754a65f22bfd
Tanmaya-Kumar-sahu <tanmayakumarsahu456@gmail.com>
2025-03-13 05:17:33 +0000

    datatypes

---
+++ 1_basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert("hello") we are using nodejs, not browser.
+
+let name = "tanmay"
+let age = 24 
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null => standalone value
+//undefined => not assingn value
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);// undefined
