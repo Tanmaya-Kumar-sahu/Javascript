86e94392175ea4047dd003b028ac8da11e15d9ae
Tanmaya-Kumar-sahu <tanmayakumarsahu456@gmail.com>
2025-03-13 05:57:36 +0000

    conversion updated

--- 1_basic/conversionOperation.js
+++ 1_basic/conversionOperation.js
@@ -26,6 +26,54 @@ let booleanIsLoggedIn = Boolean(isLoggedin)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+/*--------------------------- Operation ---------------------------------*/
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " Tanmay"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+// console.log("1" + 1);
+// console.log(1 + "2");
+// console.log("2" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3+4*5%3);//its for exam not for code
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"")
+
+
+let num1 , num2 , num3
+
+num1=num2=num3=2+2
+
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+//link to study 
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-convertion
+
+
+
+
+
+
+
 
